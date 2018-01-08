8e8b8147ea6ced552f9cb6334da0bdc38ed5526b
Ian Bowe <Ian.Bowe@dealogic.com>
2018-01-08 09:12:34 +0000

    Added

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -11,7 +11,7 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello world");
-            Console.WriteLine("Next world");
+            Console.WriteLine("Next worlds");
             Console.ReadLine();
         }
     }
